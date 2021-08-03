85511ef71fafa45ee9b6df35fa34a44904cd0271
DevSunbo <cowwhite1008@gmail.com>
2021-08-03 18:52:49 +0900

    imgae slide 미끄러지지 않는...

--- js/main.js
+++ js/main.js
@@ -1,46 +1,109 @@
-(function (window, document){
+(function (window, document) {
     const $toggles = document.querySelectorAll('.toggle'); // Return NodeList
     const $toggleBtn = document.getElementById('toggle-btn')
 
-    $toggleBtn.addEventListener('click', function (){
+    $toggleBtn.addEventListener('click', function () {
         toggleElements();
     });
 
-    window.addEventListener('resize', function (){
-        if(window.innerWidth > 768){
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 768) {
             offElements();
         }
     });
 
-    function toggleElements(){
-        [].forEach.call($toggles, function (toggle){
+    function toggleElements() {
+        [].forEach.call($toggles, function (toggle) {
             toggle.classList.toggle('on');
         });
     }
 
-    function offElements(){
-        [].forEach.call(($toggles, function (toggle){
+    function offElements() {
+        [].forEach.call(($toggles, function (toggle) {
             toggle.classList.remove('on')
         }))
     }
 })(window, document);
 
 const header = document.querySelector('.header');
+const scrollLoc = document.querySelector('.scrollLoc');
 
 window.addEventListener('scroll', () => {
     let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
-    if(scrollLocation < 80){
-        header.style.position = 'relative';
-        header.style.backgroundColor = '#6f5499';
-    }
-    else{
-        header.style.position = 'fixed';
-        header.style.backgroundColor = '#fff';
-    }
+    /* if(scrollLocation < 80){
+         header.style.position = 'relative';
+         header.style.backgroundColor = '#6f5499';
+     }
+     else{
+         header.style.position = 'fixed';
+         header.style.backgroundColor = '#fff';
+     }*/
     let screenWid = screen.width;
     let screenHgt = screen.height;
 
     let heightRatio = (scrollLocation / screenHgt) * 100;
     let currentWid = (heightRatio / 100) * screenWid;
-    
+    /*    console.log('heightRatio ', heightRatio);
+        console.log('currentWid ', currentWid);*/
+    scrollLoc.style.width = `currentWid`;
+    scrollLoc.style.border = '1px solid red';
+
 })
+
+
+const imgs = document.querySelectorAll('.izone')
+console.log(imgs);
+
+(function (window, document, undefined) {
+    const imgLeft = document.querySelector('.img-left');
+    const imgRight = document.querySelector('.img-right');
+    const img_slide = document.querySelector('.image-slide');
+    const imgs = document.querySelectorAll('.izone')
+    let count = 0;
+    imgLeft.addEventListener('click', async function  () {
+        console.log(imgs);
+        console.log(typeof imgs);
+        if (count === 0) {
+            count = 3;
+        } else {
+            count--;
+        }
+        await makeNone(imgs);
+        await makeRelav(imgs, count);
+        // imgs[count].style.display = 'relative';
+
+
+    });
+    imgRight.addEventListener('click', async function () {
+        console.log(imgs);
+       /* const slider = new Promise(
+            function (resolve, reject) {
+                if (count === 3) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+            }
+        );*/
+        if (count === 3) {
+            count = 0;
+        } else {
+            count++;
+        }
+        await makeNone(imgs);
+        await makeRelav(imgs, count);
+
+    })
+})(window, document);
+
+function makeNone (imgs) {
+    console.log('none', imgs[0]);
+    imgs.forEach(v => {
+        v.style.display = 'none';
+    })
+    return imgs;
+}
+function makeRelav (imgs, count){
+    console.log('relav', count);
+    imgs[count].style.display = 'block';
+}
